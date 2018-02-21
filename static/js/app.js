491d233150bd49339a7130021d26def683520a9d
Patrick Schuelke <p-schuelke@gmx.de>
2018-02-21 10:00:04 +0100

    refactoring: created controller class for events

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,27 @@
 
-window.onload = function () {
-  document.getElementById('navbox__burgercontainer').addEventListener('click', function () {
-    this.classList.toggle('open');
-    document.querySelector('.navbox__navbar').classList.toggle('open');
-    document.querySelector('.navbox').classList.toggle('open');
-  });
-};
+class Controller {
+  constructor() {
+    this.elements = {
+      navBurger: document.getElementById('navbox__burgercontainer'),
+      navBar: document.querySelector('.navbox__navbar'),
+      navBox: document.querySelector('.navbox')
+    };
+    this.init();
+  }
+
+  init() {
+    this.navBarListener();
+  }
+
+  navBarListener() {
+    this.elements.navBurger.addEventListener('click', function navToggle() {
+      this.classList.toggle('open');
+      this.elements.navBar.classList.toggle('open');
+      this.elements.navBox.classList.toggle('open');
+    });
+  }
+}
+
 
 // script for close navbar on scroll
 let didScroll = false;
@@ -23,9 +39,8 @@ setInterval(() => {
     document.getElementById('navbox__burgercontainer').classList.add('open');
     document.querySelector('.navbox__navbar').classList.remove('open');
     document.querySelector('.navbox').classList.add('open');
-  }
-  // closes navbar on scrolling if not on top of page
-  else if (didScroll) {
+    // closes navbar on scrolling if not on top of page
+  } else if (didScroll) {
     didScroll = false;
     document.getElementById('navbox__burgercontainer').classList.remove('open');
     document.querySelector('.navbox__navbar').classList.add('open');
@@ -42,16 +57,19 @@ function isInViewport(element) {
   const bounding = element.getBoundingClientRect();
   return (
     bounding.top >= 0
-            && bounding.left >= 0
-            && bounding.bottom <= (window.innerHeight * 1 || document.documentElement.clientHeight * 1)
-            && bounding.right <= (window.innerWidth * 1 || document.documentElement.clientWidth * 1)
+    && bounding.left >= 0
+    && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
 // triggers animation for elements that entered the viewport
 setInterval(() => {
-  for (const icon of toolsIcons) {
+  toolsIcons.forEach((icon) => {
     if (isInViewport(icon)) icon.classList.add('fadeInUpTriggered');
-  }
+  });
   if (isInViewport(contactSection)) contactSection.classList.add('slideInTriggered');
 }, 150);
+
+
+const control = new Controller();
