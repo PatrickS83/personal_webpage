5ecbbe53bec63abdd7886b45eb5584aa71f2fd7d
Patrick Schuelke <p-schuelke@gmx.de>
2018-07-04 15:25:55 +0200

    -

--- static/js/app.js
+++ static/js/app.js
@@ -21,10 +21,19 @@ class Controller {
   // fix 100vh problems in mobile browsers
   setHeroHeight() {
     const { herosite } = this.elements;
-    herosite.style.height = window.innerHeight + 'px';
-    window.addEventListener('resize', () => {
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    )
       herosite.style.height = window.innerHeight + 'px';
-    });
+    // window.addEventListener('resize', () => {
+    //   herosite.style.height = window.innerHeight + 'px';
+    // });
   }
 
   // listens to clicks on the navbar burger icon
